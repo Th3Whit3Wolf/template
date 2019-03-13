7ef1d492ef7eded4edeb41f6475bacb8adabc484
TheWhiteWolf1337 <the.white.wolf.is.1337@gmail.com>
2019-03-13 20:55:24 +0000

    Update

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ dirname=$PWD
 result="${dirname%"${dirname##*[!/]}"}" # extglob-free multi-trailing-/ trim
 result="${result##*/}"
 echo $result
-read -rep $'question name ('$result'):' PROJECT
+read -re $'question name (' -i $result -p '):' PROJECT
 echo PROJECT
 #bit u && sh -c "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/install.sh)"
+# yarn at end to initialize
