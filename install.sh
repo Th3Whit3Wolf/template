bca7007769eaef1a004c0107fee2f0fe8706c265
TheWhiteWolf1337 <the.white.wolf.is.1337@gmail.com>
2019-03-13 20:51:46 +0000

    Update

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ dirname=$PWD
 result="${dirname%"${dirname##*[!/]}"}" # extglob-free multi-trailing-/ trim
 result="${result##*/}"
 echo $result
-read -rep $'question name (${result}):' PROJECT
-
+read -rep $'question name ('$result'):' PROJECT
+echo PROJECT
 #bit u && sh -c "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/install.sh)"
