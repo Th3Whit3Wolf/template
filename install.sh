afbf93cd5ffd8b1a21712459ec58351adbc29702
TheWhiteWolf1337 <the.white.wolf.is.1337@gmail.com>
2019-03-18 14:58:18 +0000

    Update

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-Projects[0]='eslint'
-Projects[1]='eslint-config-prettier'
-Projects[2]='events'
-Projects[3]='prettier-stylelint'
-Projects[4]='stylelint-config-idiomatic-order'
-Projects[5]='tslint'
-Projects[6]='tslint-config-prettier'
-Projects[7]='tslint-plugin-prettier'
-Projects[8]='typescript'
+Projects[0]='autoprefixer'
+Projects[1]='eslint'
+Projects[2]='eslint-config-prettier'
+Projects[3]='events'
+Projects[4]='prettier-stylelint'
+Projects[5]='stylelint-config-idiomatic-order'
+Projects[6]='tslint'
+Projects[7]='tslint-config-prettier'
+Projects[8]='tslint-plugin-prettier'
+Projects[9]='typescript'
 
 Gulp[0]='gulp'
 Gulp[1]='gulp-connect'
@@ -32,25 +33,50 @@ Gulp[16]='gulp-typescript'
 Gulp[17]='gulp-prettier'
 Gulp[18]='gulp-webp'
 
+if [ -x "$(yarn)" ]; then
+    yarn init
 
-yarn init
+    for project in "${Projects[@]}"
+    do
+        yard add $project --save-dev
+    done
 
-for project in "${Projects[@]}"
-do
-    yard add -D $project
-done
+    for gulp in "${Gulp[@]}"
+    do
+        yard add $gulp --save-dev
+    done
 
-for gulp in "${Gulp[@]}"
-do
-    yard add -D $gulp
-done
+    mkdir sass
+    mkdir ts
+    mkdir -p src/css
+    mkdir src/img
+    mkdir src/js
+    echo "node_modules" > .gitignore
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/gulpfile.js)" > gulpfile.js
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tsconfig.json)" > ts/tsconfig.json
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tslint.json)" > ts/tslint.json
+elif [ -x "$(npm)" ]; then
+    npm init
 
-mkdir sass
-mkdir ts
-mkdir -p src/css
-mkdir src/img
-mkdir src/js
-echo "node_modules" > .gitignore
-echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/gulpfile.js)" > gulpfile.js
-echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tsconfig.json)" > ts/tsconfig.json
-echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tslint.json)" > ts/tslint.json
+    for project in "${Projects[@]}"
+    do
+        npm install -D $project
+    done
+
+    for gulp in "${Gulp[@]}"
+    do
+        npm install -D $gulp
+    done
+
+    mkdir sass
+    mkdir ts
+    mkdir -p src/css
+    mkdir src/img
+    mkdir src/js
+    echo "node_modules" > .gitignore
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/gulpfile.js)" > gulpfile.js
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tsconfig.json)" > ts/tsconfig.json
+    echo "$(curl -fsSL https://raw.githubusercontent.com/TheWhiteWolf1337/template/master/ts/tslint.json)" > ts/tslint.json
+else
+    echo "Npm or yarn are required to utilize this template and to use javascript."
+fi
