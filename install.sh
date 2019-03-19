4d71163a730acf50a908477c00f8f917f87753c2
TheWhiteWolf1337 <the.white.wolf.is.1337@gmail.com>
2019-03-19 18:05:17 +0000

    Update

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ BADLINESCOUNT=1
 ORIGINALFILE=$PWD/package.json
 dd if=${ORIGINALFILE} of=${ORIGINALFILE}.tmp status=none bs=1 count=$(printf "$(stat --format=%s ${ORIGINALFILE}) - $(tail -n${BADLINESCOUNT} ${ORIGINALFILE} | wc -c)\n" | bc )
 /bin/mv -f ${ORIGINALFILE}.tmp ${ORIGINALFILE}
-sed '${s/$/,/}' $PWD/package.json
+sed '${s/$/,/}' $PWD/package.json > $PWD/package.json
 echo '"scripts": {
 		"tslint": "tslint -c src/ts/tslint.json src/ts/*.ts"
 	},
